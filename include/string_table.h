e120ed5b2c055f2f2baff674fbab425d4748fd24
huangfeidian <huangfeidian@live.cn>
2019-03-25 20:41:15 +0800

    添加string table来处理字符串缓存

--- include/string_table.h
+++ include/string_table.h
@@ -12,13 +12,15 @@ namespace xlsx_reader
 	{
 		static string_table& get_instance();
 		static const std::uint16_t buffer_size = 4096 * 2;
+		static const std::uint16_t long_string_size = 4096 / 2;
 		std::vector<std::pair<char*, std::uint16_t>> buffers;
+		std::vector<std::string_view> long_strings;
 		std::unordered_set<std::size_t> buffer_set;
 		std::unordered_set<std::string_view> str_set;
 		bool contains(std::string_view source_str) const;
 		std::string_view add(std::string_view other);
 		std::string_view add(const std::string& other);
-
+		~string_table();
 
 	};
 }
